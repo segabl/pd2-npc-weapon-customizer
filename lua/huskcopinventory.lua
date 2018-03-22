153e5d4d050a771b9a0515ac19c932023cc682d6
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-03-22 15:54:13 +0100

    Fix bug with weapons as client

--- lua/huskcopinventory.lua
+++ lua/huskcopinventory.lua
@@ -1,7 +1,7 @@
 local _perform_switch_equipped_weapon_original = HuskCopInventory._perform_switch_equipped_weapon
 function HuskCopInventory:_perform_switch_equipped_weapon(weap_index, ...)
   -- check received weapon index and get the mod gun that replaces the original
-  local weap = NWC.settings.weapons[weap_index]
+  local weap = NWC.settings.weapons[tweak_data.character.weap_ids[weap_index]]
   if NWC:check_weapon(weap) then
     NWC.npc_gun_added = { id = weap.id, sync_index = weap_index, unit = self._unit }
     self._unit:inventory():add_unit_by_factory_blueprint(weap.name, true, true, weap.blueprint or tweak_data.weapon.factory[weap.name].default_blueprint, weap.cosmetics)
@@ -18,7 +18,7 @@ function HuskCopInventory:_clbk_weapon_add(data, ...)
     return
   end
   -- the same thing here again
-  local weap = NWC.settings.weapons[data.equipped_weapon_index]
+  local weap = NWC.settings.weapons[tweak_data.character.weap_ids[data.equipped_weapon_index]]
   if NWC:check_weapon(weap) then
     NWC.npc_gun_added = { id = weap.id, sync_index = data.equipped_weapon_index, unit = self._unit }
     self._unit:inventory():add_unit_by_factory_blueprint(weap.name, true, true, weap.blueprint or tweak_data.weapon.factory[weap.name].default_blueprint, weap.cosmetics)
