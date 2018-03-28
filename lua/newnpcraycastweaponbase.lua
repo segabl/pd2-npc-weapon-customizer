79d486d45389a74d97ecf66a9b4ac1cb72582083
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-03-28 16:18:17 +0200

    Add possible fix for nil value crash

--- lua/newnpcraycastweaponbase.lua
+++ lua/newnpcraycastweaponbase.lua
@@ -50,59 +50,78 @@ function NewNPCRaycastWeaponBase:auto_fire_blank(...)
   return result
 end
 
+local tweak_data_anim_play_original = NewNPCRaycastWeaponBase.tweak_data_anim_play
+function NewNPCRaycastWeaponBase:tweak_data_anim_play(anim, ...)
+  local unit_anim = self:_get_tweak_data_weapon_animation(anim)
+  -- disable animations that don't have a unit to prevent crashing
+  if NWC.settings.add_animations and not self._checked_anims[unit_anim] then
+    for part_id, data in pairs(self._parts) do
+      if data.animations and data.animations[unit_anim] and not data.unit then
+        data.animations[unit_anim] = nil
+      end
+    end
+    self._checked_anims[unit_anim] = true
+  end
+  return tweak_data_anim_play_original(self, anim, ...)
+end
+
 local setup_original = NewNPCRaycastWeaponBase.setup
 function NewNPCRaycastWeaponBase:setup(...)
   setup_original(self, ...)
 
+  self._checked_anims = {}
+
   -- adjust the stats of the newly added NPC weapon, it needs to point to the tweak data of the original gun
-  if NWC.npc_gun_added then
-    self._sync_index = NWC.npc_gun_added.sync_index
-    self._original_id = self._name_id
-    self._name_id = NWC.npc_gun_added.id
-    
-    if NWC.npc_gun_added.unit:inventory()._shield_unit_name then
-      CopInventory._chk_spawn_shield(NWC.npc_gun_added.unit:inventory(), self._unit)
-    end
-    
-    if not NWC.tweak_setups[self._name_id] then
-      if not NWC.settings.keep_sounds and not tweak_data.weapon[self._name_id].sounds.prefix:match("sniper_npc") then
-        tweak_data.weapon[self._name_id].sounds = tweak_data.weapon[self._original_id].sounds
-      end
-      tweak_data.weapon[self._name_id].muzzleflash = tweak_data.weapon[self._original_id].muzzleflash
-      tweak_data.weapon[self._name_id].shell_ejection = tweak_data.weapon[self._original_id].shell_ejection
-      if not NWC.settings.keep_types and not NWC.npc_gun_added.unit:inventory()._shield_unit_name then
-        tweak_data.weapon[self._name_id].hold = tweak_data.weapon[self._original_id].hold
-        tweak_data.weapon[self._name_id].reload = tweak_data.weapon[self._original_id].reload
-        tweak_data.weapon[self._name_id].pull_magazine_during_reload = tweak_data.weapon[self._original_id].pull_magazine_during_reload
-      end
-      NWC.tweak_setups[self._name_id] = true
-    end
-
-    self:set_ammo_max(tweak_data.weapon[self._name_id].AMMO_MAX)
-    self:set_ammo_total(self:get_ammo_max())
-    self:set_ammo_max_per_clip(tweak_data.weapon[self._name_id].CLIP_AMMO_MAX)
-    self:set_ammo_remaining_in_clip(self:get_ammo_max_per_clip())
-    
-    self._damage = tweak_data.weapon[self._name_id].DAMAGE
-
-    if not NWC:is_joker(NWC.npc_gun_added.unit) then
-      self._setup.alert_AI = not NWC.is_client
-      self._setup.alert_filter = not NWC.is_client and NWC.npc_gun_added.unit:brain().SO_access and NWC.npc_gun_added.unit:brain():SO_access()
-      self._setup.hit_slotmask = NWC.is_client and managers.slot:get_mask("bullet_impact_targets_no_AI") or managers.slot:get_mask("bullet_impact_targets") or self._bullet_slotmask
-      self._setup.hit_player = true
-      self._setup.ignore_units = {
-        NWC.npc_gun_added.unit,
-        self._unit,
-        NWC.npc_gun_added.unit:inventory()._shield_unit
-      }
-    
-      self._alert_size = tweak_data.weapon[self._name_id].alert_size
-      self._suppression = tweak_data.weapon[self._name_id].suppression
-      self._bullet_slotmask = self._setup.hit_slotmask
-      self._hit_player = self._setup.hit_player
-      self._alert_events = self._setup.alert_AI and {} or nil
-    end
-    
-    self._fire_raycast = NPCRaycastWeaponBase._fire_raycast
+  if not NWC.npc_gun_added then
+    return
   end
+
+  self._sync_index = NWC.npc_gun_added.sync_index
+  self._original_id = self._name_id
+  self._name_id = NWC.npc_gun_added.id
+  
+  if NWC.npc_gun_added.unit:inventory()._shield_unit_name then
+    CopInventory._chk_spawn_shield(NWC.npc_gun_added.unit:inventory(), self._unit)
+  end
+  
+  if not NWC.tweak_setups[self._name_id] then
+    if not NWC.settings.keep_sounds and not tweak_data.weapon[self._name_id].sounds.prefix:match("sniper_npc") then
+      tweak_data.weapon[self._name_id].sounds = tweak_data.weapon[self._original_id].sounds
+    end
+    tweak_data.weapon[self._name_id].muzzleflash = tweak_data.weapon[self._original_id].muzzleflash
+    tweak_data.weapon[self._name_id].shell_ejection = tweak_data.weapon[self._original_id].shell_ejection
+    if not NWC.settings.keep_types and not NWC.npc_gun_added.unit:inventory()._shield_unit_name then
+      tweak_data.weapon[self._name_id].hold = tweak_data.weapon[self._original_id].hold
+      tweak_data.weapon[self._name_id].reload = tweak_data.weapon[self._original_id].reload
+      tweak_data.weapon[self._name_id].pull_magazine_during_reload = tweak_data.weapon[self._original_id].pull_magazine_during_reload
+    end
+    NWC.tweak_setups[self._name_id] = true
+  end
+
+  self:set_ammo_max(tweak_data.weapon[self._name_id].AMMO_MAX)
+  self:set_ammo_total(self:get_ammo_max())
+  self:set_ammo_max_per_clip(tweak_data.weapon[self._name_id].CLIP_AMMO_MAX)
+  self:set_ammo_remaining_in_clip(self:get_ammo_max_per_clip())
+  
+  self._damage = tweak_data.weapon[self._name_id].DAMAGE
+
+  if not NWC:is_joker(NWC.npc_gun_added.unit) then
+    self._setup.alert_AI = not NWC.is_client
+    self._setup.alert_filter = not NWC.is_client and NWC.npc_gun_added.unit:brain().SO_access and NWC.npc_gun_added.unit:brain():SO_access()
+    self._setup.hit_slotmask = NWC.is_client and managers.slot:get_mask("bullet_impact_targets_no_AI") or managers.slot:get_mask("bullet_impact_targets") or self._bullet_slotmask
+    self._setup.hit_player = true
+    self._setup.ignore_units = {
+      NWC.npc_gun_added.unit,
+      self._unit,
+      NWC.npc_gun_added.unit:inventory()._shield_unit
+    }
+  
+    self._alert_size = tweak_data.weapon[self._name_id].alert_size
+    self._suppression = tweak_data.weapon[self._name_id].suppression
+    self._bullet_slotmask = self._setup.hit_slotmask
+    self._hit_player = self._setup.hit_player
+    self._alert_events = self._setup.alert_AI and {} or nil
+  end
+  
+  self._fire_raycast = NPCRaycastWeaponBase._fire_raycast
 end
